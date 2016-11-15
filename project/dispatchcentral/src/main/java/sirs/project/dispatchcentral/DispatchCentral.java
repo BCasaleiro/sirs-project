89691f92e1eac91df920263442b329465156c605
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2016-11-15 15:22:13 +0000

    added request class. added logging mechanisms

--- project/dispatchcentral/src/main/java/sirs/project/dispatchcentral/DispatchCentral.java
+++ project/dispatchcentral/src/main/java/sirs/project/dispatchcentral/DispatchCentral.java
@@ -6,6 +6,12 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.StringTokenizer
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+import Request;
 
 /**
  * Hello world!
@@ -13,46 +19,82 @@ import java.net.SocketTimeoutException;
  */
 public class DispatchCentral extends Thread
 {
-	private ServerSocket serverSocket;
-	   
-	public DispatchCentral(int port) throws IOException {
-		serverSocket = new ServerSocket(port);
+    private ServerSocket serverSocket;
+    private String inputStream;
+    private Request request;
+
+    public DispatchCentral(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+
+    private void logRequest(Request request) {
+		File file = new File("log.txt");
+
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write("[" + request.getDate() + "]\t" + request.getUserId + "\t" + request.getMessage());
+
+		bw.close();
+
+		System.out.println("[DEBUG] Logged request.");
 	}
-	
-	public void run() {
-		while(true) {
-	         try {
-	            System.out.println("[DEBUG] Waiting for client on port " + serverSocket.getLocalPort() + "...");
-	            Socket server = serverSocket.accept();
-	            
-	            System.out.println("[DEBUG] Connected to " + server.getRemoteSocketAddress());
-	            DataInputStream in = new DataInputStream(server.getInputStream());
-	            
-	            switch(in.readUTF()){
-		            case "ping":
-		            	System.out.println("[DEBUG] Client sent ping. Answered with 'pong'");
-		            	DataOutputStream out = new DataOutputStream(server.getOutputStream());
-			            out.writeUTF("pong");
-			            server.close();
-			            break;
-	            }	            
-	            
-	         }catch(SocketTimeoutException s) {
-	            System.out.println("Socket timed out!");
-	            break;
-	         }catch(IOException e) {
-	            e.printStackTrace();
-	            break;
-	         }
-	      }
-	}
-	
+
+    private Request processRequest(String request) {
+		StringTokenizer strTok = new StringTokenizer(request)
+		Request r = new Request();
+		r.setUserId(strTok.nextToken());
+		r.setMessage(strTok.nextToken())
+
+		return r;
+    }
+
+    public void run() {
+        while(true) {
+             try {
+				 System.out.println("[DEBUG] Waiting for client on port " + serverSocket.getLocalPort() + "...");
+                 Socket server = serverSocket.accept();
+
+                 System.out.println("[DEBUG] Connected to " + server.getRemoteSocketAddress());
+                 DataInputStream in = new DataInputStream(server.getInputStream());
+				 inputStream =in.readUTF();
+
+				 switch(inputStream){
+                    case "ping":
+                        System.out.println("[DEBUG] Client sent ping. Answered with 'pong'");
+                        DataOutputStream out = new DataOutputStream(server.getOutputStream());
+                        out.writeUTF("pong");
+                        server.close();
+                        break;
+                    default:
+                        System.out.println("[DEBUG] Client sent request.");
+                        request = processRequest(inputStream);
+						logRequest(request);
+
+						out.writeUTF("Help is on the way!");
+						server.close();
+                }
+
+             }catch(SocketTimeoutException s) {
+                System.out.println("Socket timed out!");
+                break;
+             }catch(IOException e) {
+                e.printStackTrace();
+                break;
+             }
+          }
+    }
+
     public static void main( String[] args )
     {
-    	int port = Integer.parseInt(args[0]);
+        int port = Integer.parseInt(args[0]);
         try {
-        	Thread t = new DispatchCentral(port);
-        	t.start();
+            Thread t = new DispatchCentral(port);
+            t.start();
         }catch(IOException e) {
            e.printStackTrace();
         }
---
+++ project/dispatchcentral/src/main/java/sirs/project/dispatchcentral/Request.java
@@ -0,0 +1,50 @@
+import java.util.Date;
+
+public class Request {
+    private String userId;
+    private Date date;
+    private String message;
+    private boolean dispatched;
+
+    public Request() {
+        this.date = Date();
+    }
+
+    public Request(String userId, String message) {
+        this.userId = userId;
+        this.message = message;
+        this.date = Date();
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isDispatched() {
+        return dispatched;
+    }
+
+    public void setDispatched(boolean dispatched) {
+        this.dispatched = dispatched;
+    }
+}
