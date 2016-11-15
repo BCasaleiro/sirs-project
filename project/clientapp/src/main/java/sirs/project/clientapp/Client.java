ae7962bb56619d79caa4af9888d387ce83fc6dc2
Miguel Pinto <miguelpinto25@hotmail.com>
2016-11-15 15:10:09 +0000

    Added sendRequest method

--- project/clientapp/src/main/java/sirs/project/clientapp/Client.java
+++ project/clientapp/src/main/java/sirs/project/clientapp/Client.java
@@ -18,6 +18,7 @@ public class Client
 	private DataInputStream in = null;
 	private OutputStream outServer = null;
 	private DataOutputStream out = null;
+	private String phoneNumber = null;
 	
 	public Client(String serverName, int port) throws UnknownHostException, IOException{
 		client = new Socket(serverName, port);
@@ -35,6 +36,14 @@ public class Client
 		out.writeUTF("ping");    
         System.out.println(in.readUTF() + "\n");
 	}
+
+	public void setPhoneNumber(String number){
+		phoneNumber = number;
+	}
+	
+	private void sendRequest() throws IOException{
+		out.writeUTF(phoneNumber + ",HELP!");
+	}
 	
 	private void clearScreen(){
 		final String ANSI_CLS = "\u001b[2J";
@@ -43,27 +52,28 @@ public class Client
         System.out.flush();
 	}
 	
-    public static void main( String[] args )
-    {
-        String serverName = args[0];
+    public static void main(String[] args ){
+    	String serverName = args[0];
         int port = Integer.parseInt(args[1]);
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
         Client clientClass = null;
 		try {
 			clientClass = new Client(serverName, port);
+			clientClass.clearScreen();
+			System.out.print("Insert your phone number: ");
+			clientClass.setPhoneNumber(br.readLine());
 		} catch (IOException e) {
 			System.err.println("[ERROR] Failed to initialize client");
 			return ;
 		}
         
         try{
-        	System.out.println("[DEBUG] Connecting to " + serverName + " on port " + port);
-        	
+        	System.out.println("[DEBUG] Connecting to " + serverName + " on port " + port);      	
         	System.out.println("[DEBUG] Successfully connected to server");
         	
         	String switchCase = null;
-        	clientClass.clearScreen();
-        	
+
         	do{
             	System.out.println("Choose one option from the following");
             	System.out.println("-----------------------------------");
@@ -71,7 +81,7 @@ public class Client
             	System.out.println("2) Send Request");
             	System.out.println("3) Exit");
             	
-            	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            	
             	switchCase = br.readLine();
             	
             	switch(switchCase){
@@ -79,6 +89,7 @@ public class Client
     	        		clientClass.ping();
     	        		break;
     	        	case "2":
+    	        		clientClass.sendRequest();
     	        		break;
             	}            	
         	} while(!switchCase.equals("3"));
