f49c270f3f2b6b716b9b78bef86d5a8aab293b1b
Miguel Pinto <miguelpinto25@hotmail.com>
2016-11-22 14:47:23 +0000

    Big updates! [REVIEW]

--- project/dispatchcentral/src/main/java/sirs/project/dispatchcentral/DispatchCentral.java
+++ project/dispatchcentral/src/main/java/sirs/project/dispatchcentral/DispatchCentral.java
@@ -1,27 +1,191 @@
 package sirs.project.dispatchcentral;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketTimeoutException;
-import java.util.StringTokenizer;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.util.Date;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.*;
+import java.util.StringTokenizer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
-* Hello world!
-*
-*/
-public class DispatchCentral extends Thread
+public class DispatchCentral{
+	
+	/*
+	 * Variables
+	 */
+	
+	final static String PROJ_DIR = System.getProperty("user.dir");
+	final Logger log = LoggerFactory.getLogger(DispatchCentral.class);
+	
+	private static DispatchCentral server; 
+    private ServerSocket serverSocket;
+    private ExecutorService executorService = Executors.newFixedThreadPool(10);
+    
+    private Connection c = null;
+    private DatabaseConstants dbConstants = null;
+    private DatabaseFunctions dbFunctions = null;
+    
+    /*
+     * Main method
+     */   
+    public static void main(String[] args) throws IOException {
+    	
+    	PropertyConfigurator.configure(PROJ_DIR + "/log4j.properties");
+        server = new DispatchCentral();
+        int port = Integer.parseInt(args[0]);
+        server.checkConnectivity();
+        server.runServer(port);
+    }
+    
+    /*
+     * Database functions
+     */
+    public void createNecessaryTables() {
+        dbFunctions.createTable(c, dbConstants.requestsTableCreation);
+        dbFunctions.createTable(c, dbConstants.ratingsTableCreation);
+
+        //Just for test
+        //dbTestingFunction();
+    }
+    
+    public void dbTestingFunction() {
+        dbFunctions.insertUser(c, dbConstants.insertUser, "911111111");
+        dbFunctions.updateRating(c, dbConstants.updateRating, "911111111", 20);
+        System.out.println(dbFunctions.userExists(c, dbConstants.listPhoneNumbers, "9123213"));
+        System.out.println(dbFunctions.userRating(c, dbConstants.userRating, "911111111"));
+        
+    }
+    
+    public void checkConnectivity(){
+    	dbConstants = new DatabaseConstants();
+    	if( connectToDatabase() == 1 ) {
+	      dbFunctions = new DatabaseFunctions(dbConstants);
+	      createNecessaryTables();
+	      System.out.println("Created necessary tables");
+	    } else {
+	      System.out.println("Error Connecting to Database");
+	    }
+    }
+    
+    public int connectToDatabase() {
+        try {
+          Class.forName("org.postgresql.Driver");
+          c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/emergenciesdb",
+          "postgres", "123");
+        }catch (Exception e) {
+          e.printStackTrace();
+          System.err.println(e.getClass().getName()+": "+e.getMessage());
+          System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+        return 1;
+      }
+    
+    /*
+     * Function that will wait for incoming connections
+     */
+    private void runServer(int serverPort) {        
+        try {
+            log.info("Starting Server in port " + serverPort);
+            serverSocket = new ServerSocket(serverPort); 
+
+            while(true) {
+                try {
+                    Socket s = serverSocket.accept();
+                    log.info("Connection accepted from " + s.getInetAddress().getHostAddress());
+                    executorService.submit(new ServiceRequest(s));
+                } catch(IOException ioe) {
+                    log.error("Error accepting connection");
+                    log.error(ioe.getMessage());
+                }
+            }
+        }catch(IOException e) {
+            log.error("Error starting Server on port " + serverPort);
+            log.error(e.getMessage());
+        }
+    }
+
+    /*
+     * Function to be called to shutdown the server
+     */
+    private void stopServer() {
+        executorService.shutdownNow();
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            log.error("Error while closing the server");
+            log.error(e.getMessage());
+        }
+        System.exit(0);
+    }
+    
+    /*
+     * Nested class to process the requests
+     */
+    class ServiceRequest implements Runnable {
+
+        private Socket socket;
+        private Connection c = null;
+        private DatabaseConstants dbConstants = null;
+        private DatabaseFunctions dbFunctions = null;
+
+        public ServiceRequest(Socket connection) {
+            this.socket = connection;
+        }
+
+        private Request processRequest(String request) {
+            StringTokenizer strTok = new StringTokenizer(request, ",");
+            Request r = new Request();
+            r.setUserId(strTok.nextToken());
+            r.setMessage(strTok.nextToken());
+
+            return r;
+        }
+        
+        public void testRequestdbFunctions(Request request) {
+        	dbFunctions.insertRequest(c, dbConstants.insertRequest, request);
+        	int id = dbFunctions.getRequestId(c, dbConstants.getRequestId, request);
+        	dbFunctions.setDispatched(c, dbConstants.setDispatched, id);
+        }
+
+        public void run(){
+        	
+        	log.info("Started processing the request");
+        	try(
+        		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        	){
+        		String message = null;
+        		if((message = in.readLine()) != null){
+        			Request request = processRequest(message);
+	    	        log.info(request.getUserId() + ", " + request.getMessage());
+        			//insert on db
+	    	        //testRequestdbFunctions(request);
+	            	//dbFunctions.insertRequest(c, dbConstants.insertRequest, request);
+
+        			System.out.println(message);
+        			out.println("Help is on the way");
+        		}
+        	}catch(IOException e){
+        		log.error(e.getMessage());
+        	}
+        }        
+    }
+	
+}
+
+
+/**public class DispatchCentral extends Thread
 {
   final Logger logger = LoggerFactory.getLogger(DispatchCentral.class);
 
@@ -96,41 +260,45 @@ public class DispatchCentral extends Thread
   }
 
   public void run() {
-    while(true) {
-      try {
-        System.out.println("[DEBUG] Waiting for client on port " + serverSocket.getLocalPort() + "...");
-        Socket server = serverSocket.accept();
-
-        System.out.println("[DEBUG] Connected to " + server.getRemoteSocketAddress());
-        DataInputStream in = new DataInputStream(server.getInputStream());
-        DataOutputStream out = new DataOutputStream(server.getOutputStream());
-        inputStream =in.readUTF();
-
-        switch(inputStream){
-          case "ping":
-          System.out.println("[DEBUG] Client sent ping. Answered with 'pong'");
-          out.writeUTF("pong");
-          server.close();
-          break;
-          default:
-          System.out.println("[DEBUG] Client sent request.");
-          request = processRequest(inputStream);
-          logRequest(request);
-          //insert on db
-          testRequestdbFunctions(request);
-          
-          out.writeUTF("Help is on the way!");
-          server.close();
-        }
-
-      }catch(SocketTimeoutException s) {
-        System.out.println("Socket timed out!");
-        break;
-      }catch(IOException e) {
-        e.printStackTrace();
-        break;
-      }
-    }
+	Socket server = null;
+	while(true) {
+	  try {
+	    System.out.println("[DEBUG] Waiting for client on port " + serverSocket.getLocalPort() + "...");
+	    server = serverSocket.accept();
+	
+	    System.out.println("[DEBUG] Connected to " + server.getRemoteSocketAddress());
+	    DataInputStream in = new DataInputStream(server.getInputStream());
+	    DataOutputStream out = new DataOutputStream(server.getOutputStream());
+	    inputStream =in.readUTF();
+	
+	    switch(inputStream){
+	      case "ping":
+	          System.out.println("[DEBUG] Client sent ping. Answered with 'pong'");
+	          out.writeUTF("pong");          
+	          break;
+	      default:
+	          System.out.println("[DEBUG] Client sent request.");
+	          request = processRequest(inputStream);
+	          logRequest(request);
+	          //insert on db
+	          testRequestdbFunctions(request);
+	          
+	          out.writeUTF("Help is on the way!");
+	    }
+	
+	  }catch(SocketTimeoutException s) {
+	    System.out.println("Socket timed out!");
+	    break;
+	  }catch(IOException e) {
+	    e.printStackTrace();
+	    break;
+	  }
+	}
+	try {
+		server.close();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
   }
 
   public static void main( String[] args ) {
@@ -142,4 +310,4 @@ public class DispatchCentral extends Thread
       e.printStackTrace();
     }
   }
-}
+}**/
