c3c7afaa612b4f6558a08a0c1a0d68b2e5a5d8ef
Miguel Pinto <miguelpinto25@hotmail.com>
2016-11-28 08:44:19 +0000

    Added Certificate Authority project

---
+++ project/ca/src/main/java/sirs/project/ca/CA.java
@@ -0,0 +1,10 @@
+package sirs.project.ca;
+
+public class CA {
+
+    public static void main(String[] args ){
+        int port = Integer.parseInt(args[0]);
+        
+        System.out.println("Hello world!");
+    }
+}
