81ee6e30f6dd8dda8a53bf486fd088aba80796d2
Miguel Pinto <miguelpinto25@hotmail.com>
2016-11-28 22:06:19 +0000

    Added communication through SSL socket for CA and DC

--- project/clientapp/src/main/java/sirs/project/clientapp/Client.java
+++ project/clientapp/src/main/java/sirs/project/clientapp/Client.java
@@ -1,32 +1,34 @@
 package sirs.project.clientapp;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.net.Socket;
 import java.net.UnknownHostException;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 public class Client 
 {
 	
-	private InputStream inFromServer = null;
-	private Socket client = null;
-	private DataInputStream in = null;
-	private OutputStream outServer = null;
-	private DataOutputStream out = null;
+	private SSLSocket ssldispatchsocket = null;
+	private SSLSocket sslcasocket = null;
 	private String phoneNumber = null;
 	
-	public Client(String serverName, int port) throws UnknownHostException, IOException{
-		client = new Socket(serverName, port);		
+	public Client(String serverName, int dispatchport, int caport) throws UnknownHostException, IOException{
+		SSLSocketFactory factory=(SSLSocketFactory) SSLSocketFactory.getDefault();
+        try {
+        	ssldispatchsocket = (SSLSocket) factory.createSocket(serverName, dispatchport);
+			sslcasocket = (SSLSocket) factory.createSocket(serverName, caport);
+        }catch(IOException e){
+        	e.printStackTrace();
+        }
 	}
 	
 	private void exitConnection() throws IOException{
-		client.close();
+		ssldispatchsocket.close();
+		sslcasocket.close();
 	}
 	
 	public void setPhoneNumber(String number){
@@ -34,9 +36,9 @@ public class Client
 	}
 	
 	private void sendRequest() throws IOException{
-		 PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+		 PrintWriter out = new PrintWriter(ssldispatchsocket.getOutputStream(), true);
 		 out.println(phoneNumber + ",HELP!");
-		 BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		 BufferedReader in = new BufferedReader(new InputStreamReader(ssldispatchsocket.getInputStream()));
 			 
 		 String fromServer = in.readLine();
 		 if(fromServer != null){
@@ -51,15 +53,32 @@ public class Client
         System.out.flush();
 	}
 	
+	private void contactCA(){
+        try {
+			PrintWriter out = new PrintWriter(sslcasocket.getOutputStream(), true);
+    		BufferedReader in = new BufferedReader(new InputStreamReader(sslcasocket.getInputStream()));
+    		out.println("Primeiro contacto");
+    		String message = null;
+    		if((message = in.readLine()) != null){
+    			System.out.println(message);
+    		}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+	
     public static void main(String[] args ){
     	String serverName = args[0];
-        int port = Integer.parseInt(args[1]);
+        int dispatchport = Integer.parseInt(args[1]);
+        int caport = Integer.parseInt(args[2]);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       
         Client clientClass = null;
 		try {
-			clientClass = new Client(serverName, port);
+			clientClass = new Client(serverName, dispatchport, caport);
 			clientClass.clearScreen();
+			System.out.println("[DEBUG] Contacting Certificate Authority");
+			clientClass.contactCA();
 			System.out.print("Insert your phone number: ");
 			clientClass.setPhoneNumber(br.readLine());
 		} catch (IOException e) {
@@ -68,8 +87,8 @@ public class Client
 		}
         
         try{
-        	System.out.println("[DEBUG] Connecting to " + serverName + " on port " + port);      	
-        	System.out.println("[DEBUG] Successfully connected to server");
+        	System.out.println("[DEBUG] Connecting to " + serverName + " on port " + dispatchport);      	
+        	System.out.println("[DEBUG] Successfully connected to dispatch central");
         	
         	System.out.println("Choose one option from the following");
         	System.out.println("-----------------------------------");
