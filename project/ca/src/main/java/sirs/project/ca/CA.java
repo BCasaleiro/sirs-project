adf6e17ac16af42cc8859e5c17e036043b066a50
Miguel Pinto <miguelpinto25@hotmail.com>
2016-11-29 15:34:57 +0000

    CA receives certificate from client

--- project/ca/src/main/java/sirs/project/ca/CA.java
+++ project/ca/src/main/java/sirs/project/ca/CA.java
@@ -1,11 +1,18 @@
 package sirs.project.ca;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -77,10 +84,10 @@ public class CA {
      */
     class ServiceRequest implements Runnable {
 
-        private Socket socket;
+        private SSLSocket sslsocket;
 
-        public ServiceRequest(Socket connection) {
-            this.socket = connection;
+        public ServiceRequest(SSLSocket connection) {
+            this.sslsocket = connection;
         }
 
         private void processRequest(String request) {
@@ -91,15 +98,36 @@ public class CA {
         	
         	log.info("Started processing the request");
         	try(
-        		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        		//PrintWriter out = new PrintWriter(sslsocket.getOutputStream(), true);
+        		//BufferedReader in = new BufferedReader(new InputStreamReader(sslsocket.getInputStream()));
+        		ObjectInputStream fromClient = new ObjectInputStream(sslsocket.getInputStream());
+        		ObjectOutputStream toClient = new ObjectOutputStream(sslsocket.getOutputStream());
         	){
-        		String message = null;
+        		String message = (String)fromClient.readObject();
+        		if(message.equals("Sending Certificate")){
+        			toClient.writeObject("Proceed");
+        			byte[] cert = (byte[])fromClient.readObject();
+            		java.security.cert.Certificate certificate = CertificateFactory.getInstance("X.509").generateCertificate(new ByteArrayInputStream(cert));
+            		System.out.println(certificate);
+        		}
+        		
+        		
+        		
+        		/*String message = null;
         		if((message = in.readLine()) != null){
         			System.out.println(message);
+        			try {
+						CertificateFactory cf = CertificateFactory.getInstance("X.509");
+						InputStream inStream = new ByteArrayInputStream(message.getBytes());
+						Certificate cert = cf.generateCertificate(inStream);
+						System.out.println(cert);
+					} catch (CertificateException e) {
+						e.printStackTrace();
+					}
+        			System.out.println(message);
         			out.println("Mensagem recebida!!!!");
-        		}
-        	}catch(IOException e){
+        		}*/
+        	}catch(IOException | ClassNotFoundException | CertificateException e){
         		log.error(e.getMessage());
         	}
         }        
