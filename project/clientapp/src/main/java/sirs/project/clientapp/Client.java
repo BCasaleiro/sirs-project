e3930d39585fd8bbf69833e14f0187abef1792b7
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2016-11-29 14:11:55 +0000

    added unique identifier to the request hashing the phone number with the time of the request

--- project/clientapp/src/main/java/sirs/project/clientapp/Client.java
+++ project/clientapp/src/main/java/sirs/project/clientapp/Client.java
@@ -4,18 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 import java.net.UnknownHostException;
+
+import java.util.Date;
+import java.util.Base64;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
-public class Client 
+
+public class Client
 {
-	
+
 	private SSLSocket ssldispatchsocket = null;
 	private SSLSocket sslcasocket = null;
 	private String phoneNumber = null;
-	
+
 	public Client(String serverName, int dispatchport, int caport) throws UnknownHostException, IOException{
 		SSLSocketFactory factory=(SSLSocketFactory) SSLSocketFactory.getDefault();
         try {
@@ -25,34 +34,49 @@ public class Client
         	e.printStackTrace();
         }
 	}
-	
+
+	private String hashText(String text) {
+		try{
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(text.getBytes("UTF-8"));
+			byte[] digest = md.digest();
+			return new String(Base64.getEncoder().encodeToString(digest));
+		} catch(NoSuchAlgorithmException|UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
 	private void exitConnection() throws IOException{
 		ssldispatchsocket.close();
 		sslcasocket.close();
 	}
-	
+
 	public void setPhoneNumber(String number){
 		phoneNumber = number;
 	}
-	
+
 	private void sendRequest() throws IOException{
 		 PrintWriter out = new PrintWriter(ssldispatchsocket.getOutputStream(), true);
-		 out.println(phoneNumber + ",HELP!");
+		 String id = hashText(phoneNumber + (new Date()).getTime());
+		 System.out.println(id);
+		 out.println( id + "," +  phoneNumber + ",HELP!");
 		 BufferedReader in = new BufferedReader(new InputStreamReader(ssldispatchsocket.getInputStream()));
-			 
+
 		 String fromServer = in.readLine();
 		 if(fromServer != null){
 			 System.out.println(fromServer);
 		 }
 	}
-	
+
 	private void clearScreen(){
 		final String ANSI_CLS = "\u001b[2J";
         final String ANSI_HOME = "\u001b[H";
         System.out.print(ANSI_CLS + ANSI_HOME);
         System.out.flush();
 	}
-	
+
 	private void contactCA(){
         try {
 			PrintWriter out = new PrintWriter(sslcasocket.getOutputStream(), true);
@@ -64,15 +88,15 @@ public class Client
     		}
 		} catch (IOException e) {
 			e.printStackTrace();
-		}		
+		}
 	}
-	
+
     public static void main(String[] args ){
     	String serverName = args[0];
         int dispatchport = Integer.parseInt(args[1]);
         int caport = Integer.parseInt(args[2]);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      
+
         Client clientClass = null;
 		try {
 			clientClass = new Client(serverName, dispatchport, caport);
@@ -85,26 +109,26 @@ public class Client
 			System.err.println("[ERROR] Failed to initialize client");
 			return ;
 		}
-        
+
         try{
-        	System.out.println("[DEBUG] Connecting to " + serverName + " on port " + dispatchport);      	
+        	System.out.println("[DEBUG] Connecting to " + serverName + " on port " + dispatchport);
         	System.out.println("[DEBUG] Successfully connected to dispatch central");
-        	
+
         	System.out.println("Choose one option from the following");
         	System.out.println("-----------------------------------");
         	System.out.println("1) Send Request");
-        	System.out.println("2) Exit");        	
-        	
+        	System.out.println("2) Exit");
+
         	String switchCase = br.readLine();
-        	
+
         	switch(switchCase){
 	        	case "1":
 	        		clientClass.sendRequest();
 	        		break;
 	        	default:
 	        		break;
-        	}            	
-        		
+        	}
+
             clientClass.exitConnection();
         }catch(IOException e){
         	e.printStackTrace();
